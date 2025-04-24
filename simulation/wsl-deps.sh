17e2334a94d4ead67daa129136820e43e9d2a926
rodrigoorozco <rodrigoorozco025@gmail.com>
2025-04-24 15:01:34 -0500

    run all benchmarks seq script & WSL dependencies

---
+++ simulation/wsl-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# to run this script: bash wsl-deps.sh
+
+sudo apt install zip
+sudo apt install python3.10-venv
+sudo apt install zlib1g-dev
+
+chmod +x ./zsim/run-simulation
+chmod +x ./zsim/run-all-xpolicy
+chmod +x /$(pwd)/tools/pin-2.14-71313-gcc.4.4.7-linux/intel64/bin/pinbin
+
+echo ""
+echo "WSL configuration complete"
--- simulation/zsim/wsl-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt install zip
-sudo apt install python3.10-venv
-sudo apt install zlib1g-dev
-
-chmod +x ./run-simulation
-chmod +x ./run-all-xpolicy
-chmod +x /$(pwd)/../tools/pin-2.14-71313-gcc.4.4.7-linux/intel64/bin/pinbin
